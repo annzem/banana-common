9f567218cdc4f3e5dd12fb0ac7f945e74fa3ce44
annzem <annzemlinsky@gmail.com>
2022-04-09 02:38:56 +0300

    add deployment, add tgmessage

---
+++ src/main/java/com/github/annzem/banana/protocol/TgMessage.java
@@ -0,0 +1,25 @@
+package com.github.annzem.banana.protocol;
+
+public class TgMessage {
+
+    private String chatId;
+
+    private String text;
+
+
+    public String getChatId() {
+        return chatId;
+    }
+
+    public void setChatId(String chatId) {
+        this.chatId = chatId;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+}
